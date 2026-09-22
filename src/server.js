7d05200ac5990ca05f7db7624e5d7d29184bc856
Дмитрий Гавриш <demetriusgarson@gmail.com>
2026-09-22 16:08:46 +0300

    remove test error

--- src/server.js
+++ src/server.js
@@ -21,10 +21,6 @@ app.get('/', (req, res) => {
 
 app.use(notesRoutes);
 
-// app.get('/test-error', (req, res) => {
-//   throw new Error('Simulated server error');
-// });
-
 app.use(notFoundHandler);
 
 app.use(errorHandler);
